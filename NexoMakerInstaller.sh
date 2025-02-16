070da2421cb02b1c6f94181886680d2d18e2bd65
Terrocraft <100000541+Terrocraft@users.noreply.github.com>
2025-02-16 02:17:13 +0100

    Update NexoMakerInstaller.sh
    Set to a latest download link.

--- NexoMakerInstaller.sh
+++ NexoMakerInstaller.sh
@@ -8,8 +8,8 @@ export WINEPREFIX=$HOME/64bitprefix
 
 mkdir -p "$WINEPREFIX"
 
-NEXO_URL="https://www.dropbox.com/scl/fi/s0zhfxttlvvwco8e69e1e/nexo-maker-0.0.1-setup.exe?rlkey=xdktt6a88ixxbuztr50t91e94&st=5pmlzzgn&dl=1"
-NEXO_PATH="$HOME/nexo-maker-0.0.1-setup.exe"
+NEXO_URL="https://www.dropbox.com/scl/fi/s0zhfxttlvvwco8e69e1e/nexo-maker.exe?rlkey=xdktt6a88ixxbuztr50t91e94&st=wj2zjgsp&dl=1"
+NEXO_PATH="$HOME/nexo-maker.exe"
 wget -O "$NEXO_PATH" "$NEXO_URL"
 
 WINEARCH=win64 WINEPREFIX=~/64bitprefix wine "$NEXO_PATH"
